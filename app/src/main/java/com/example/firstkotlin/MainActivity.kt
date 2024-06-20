f66892c20226232215f0ea846d65298ebcfe34f9
Beneta-Johnson <beneta.johnson2022@vitstudent.ac.in>
2024-06-20 21:46:08 +0530

    Assignment-4

--- app/src/main/java/com/example/firstkotlin/MainActivity.kt
+++ app/src/main/java/com/example/firstkotlin/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.firstkotlin
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,4 +20,27 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
+        val Namebox= findViewById<EditText>(R.id.NameBox)
+        val Passbox= findViewById<EditText>(R.id.PasswordBox)
+        val button = findViewById<Button>(R.id.Loginbutton)
+        var UserName=""
+        var UserPass=""
+        button.setOnClickListener{
+            UserName= Namebox.text.toString()
+            UserPass= Passbox.text.toString()
+            if (UserName== ""){
+                Toast.makeText(this@MainActivity,"The Username is empty", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                if(UserPass=="BowBowDoggy1103"){
+                    val intent= Intent(this,MainActivity2::class.java)
+                    intent.putExtra("Name",UserName)
+                    startActivity(intent)
+                }
+                else{
+                    Toast.makeText(this@MainActivity,"Password is incorrect", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    }}
