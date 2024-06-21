a782061411d5fb64daec35e30165ac05c9113e8b
Beneta-Johnson <beneta.johnson2022@vitstudent.ac.in>
2024-06-21 13:14:30 +0530

    Intent Dial

--- app/src/main/java/com/example/firstkotlin/MainActivity.kt
+++ app/src/main/java/com/example/firstkotlin/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.firstkotlin
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -21,5 +22,7 @@ class MainActivity : AppCompatActivity() {
     }
     fun clickHandler(view: View) {
         Log.i("MainActivity-clickhandler","button clicked")
+        var dialIntent: Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:98765432"))  //intent= intention
+        startActivity(dialIntent)
     }
 }
