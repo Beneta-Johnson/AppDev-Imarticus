9ab71ad44f8d9dfc95f583a2c3c9ea23b3fc1496
Beneta-Johnson <beneta.johnson2022@vitstudent.ac.in>
2024-06-21 17:45:09 +0530

    Get images through coil library

--- app/src/main/java/com/examples/HomeActivity.kt
+++ app/src/main/java/com/examples/HomeActivity.kt
@@ -3,12 +3,14 @@ package com.examples
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
 import com.example.firstkotlin.R
 import com.examples.MarsApi
 import com.examples.MarsPhoto
@@ -18,16 +20,18 @@ import kotlinx.coroutines.launch
 import okhttp3.Dispatcher
 
 class HomeActivity : AppCompatActivity(){
-    var TAG = HomeActivity::class.java.simpleName    //"HomeActivity"
+    var TAG = HomeActivity::class.java.simpleName
 
     lateinit var marsRecyclerView:RecyclerView
     lateinit var marsAdapter: MarsAdapter
     lateinit var photos:List<MarsPhoto>
+    lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
+        imageView = findViewById(R.id.imageView)
         marsRecyclerView = findViewById(R.id.recyclerViewUrls)
         marsRecyclerView.layoutManager = LinearLayoutManager(this)
         photos = ArrayList()
@@ -49,6 +53,8 @@ class HomeActivity : AppCompatActivity(){
             var listMarsPhotos =   MarsApi.retrofitService.getPhotos()
             // photos = listMarsPhotos
             marsAdapter.listMarsPhotos = listMarsPhotos
+            //import coil.load
+            imageView.load(listMarsPhotos.get(0).imgSrc)
             marsAdapter.notifyDataSetChanged()
             //   var tvHome:TextView = findViewById(R.id.tvHome)
 //            tvHome.setText(listMarsPhotos.get(1).imgSrc)
--- app/src/main/java/com/examples/MarsPhoto.kt
+++ app/src/main/java/com/examples/MarsPhoto.kt
@@ -1,12 +1,18 @@
 package com.examples
 
+import com.squareup.moshi.Json
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-@Serializable
+//serialization = baloon - defllate - inflate --make the object  transport ready
+//inflate -- deserialzation  -- autocad - serial nos on the floppy
+
+//break down your project into independently testable tasks -- small, discreet, independent
+
+
 data class MarsPhoto(
     val id: String,
-    @SerialName(value = "img_src")
+    @Json(name = "img_src")
     val imgSrc: String
 
 )
