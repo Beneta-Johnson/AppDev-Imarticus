df011c7bbb2325acea88ebb10d7e64ff60fda5e6
Beneta-Johnson <beneta.johnson2022@vitstudent.ac.in>
2024-06-21 16:19:26 +0530

    Step in and Step out (functions)

--- app/src/main/java/com/example/firstkotlin/MainActivity.kt
+++ app/src/main/java/com/example/firstkotlin/MainActivity.kt
@@ -57,10 +57,30 @@ class MainActivity : AppCompatActivity() {
            var webIntent: Intent = Intent(Intent.ACTION_VIEW,Uri.parse("http://www.ndtv.com"))
            startActivity(webIntent)*/
 
+        var c = 10 + 20
+        add(10,20)
         var hIntent:Intent = Intent(this,HomeActivity::class.java)
         hIntent.putExtra("mykey","android-Beneta")
         throw NullPointerException("Demo VIT Exception")
         startActivity(hIntent)
+    }
+
+    private fun add(i: Int, i1: Int):Int {
+        var c = 20
+        var d = 20 * i
+        mul(5,4)
+        return i +i1
+
+    }
+
+    private fun mul(i: Int, i1: Int) {
+        val d = i * i1
+        div(6,3)
+
+    }
+
+    private fun div(i: Int, i1: Int) {
+        add(10,20)
     }
     fun inflateXml(){
         var nameEditText = EditText(this)
