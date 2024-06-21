7e2c84ba6bd02986f2bb24045fdd36ac2f5417c7
Beneta-Johnson <beneta.johnson2022@vitstudent.ac.in>
2024-06-21 16:12:16 +0530

    Lifecycle of an activity

--- app/src/main/java/com/example/firstkotlin/MainActivity.kt
+++ app/src/main/java/com/example/firstkotlin/MainActivity.kt
@@ -14,13 +14,38 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.examples.HomeActivity
 
+
 class MainActivity : AppCompatActivity() {
+    var TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        Log.i(TAG,"activity is getting created")
 
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e(TAG,"activity is started")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.w(TAG,"activity has paused")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"activity is stopped")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v(TAG,"activity is destroyed")
+    }
+
     fun clickHandler(view: View) {
         Log.i("MainActivity-clickhandler","button clicked")
         /*  var dialIntent: Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:98765432"))  //intent= intention
@@ -31,4 +56,13 @@ class MainActivity : AppCompatActivity() {
         hIntent.putExtra("mykey","android-Beneta")
         startActivity(hIntent)
     }
+    fun inflateXml(){
+        var nameEditText = EditText(this)
+        nameEditText.setHint("enter ur name")
+        var pwdEditText = EditText(this)
+        pwdEditText.setHint("enter ur pass")
+        var loginButton = Button(this)
+        loginButton.setText("login")
+    }
+
 }
